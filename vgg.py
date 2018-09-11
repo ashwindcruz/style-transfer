edd63cafbaf860c2fd4cbf45f153702d05f36ab7
CodeOwl94 <ashwin@bloom.org.au>
2018-09-11 19:46:33 +0100

    Add new function to only have feature extraction layers

--- vgg.py
+++ vgg.py
@@ -275,26 +275,62 @@ def vgg_19(inputs,
       net = slim.max_pool2d(net, [2, 2], scope='pool5')
 
       # Use conv2d instead of fully_connected layers.
-      # net = slim.conv2d(net, 4096, [7, 7], padding=fc_conv_padding, scope='fc6')
-      # net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
-      #                    scope='dropout6')
-      # net = slim.conv2d(net, 4096, [1, 1], scope='fc7')
+      net = slim.conv2d(net, 4096, [7, 7], padding=fc_conv_padding, scope='fc6')
+      net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
+                         scope='dropout6')
+      net = slim.conv2d(net, 4096, [1, 1], scope='fc7')
       # # Convert end_points_collection into a end_point dict.
       end_points = slim.utils.convert_collection_to_dict(end_points_collection)
-      # if global_pool:
-      #  net = tf.reduce_mean(net, [1, 2], keep_dims=True, name='global_pool')
-      #  end_points['global_pool'] = net
-      # if num_classes:
-      #   net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
-      #                      scope='dropout7')
-      #   net = slim.conv2d(net, num_classes, [1, 1],
-      #                     activation_fn=None,
-      #                     normalizer_fn=None,
-      #                     scope='fc8')
-      #   if spatial_squeeze:
-      #     net = tf.squeeze(net, [1, 2], name='fc8/squeezed')
-      #   end_points[sc.name + '/fc8'] = net
+      if global_pool:
+       net = tf.reduce_mean(net, [1, 2], keep_dims=True, name='global_pool')
+       end_points['global_pool'] = net
+      if num_classes:
+        net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
+                           scope='dropout7')
+        net = slim.conv2d(net, num_classes, [1, 1],
+                          activation_fn=None,
+                          normalizer_fn=None,
+                          scope='fc8')
+        if spatial_squeeze:
+          net = tf.squeeze(net, [1, 2], name='fc8/squeezed')
+        end_points[sc.name + '/fc8'] = net
       return net, end_points
+
+def vgg_19_conv(inputs,
+           scope='vgg_19'):
+  """Oxford Net VGG 19-Layers version E Example, simplified example
+
+  All fully-connected layers have been removed. 
+  Only conv layers present. 
+  This is essentially just the feature extractor section of the network. 
+
+  Args:
+    inputs: a tensor of size [batch_size, height, width, channels].
+    scope: Optional scope for the variables.
+
+  Returns:
+    end_points: a dict of tensors with intermediate activations.
+  """
+  with tf.variable_scope(scope, 'vgg_19', [inputs]) as sc:
+    end_points_collection = sc.original_name_scope + '_end_points'
+    # Collect outputs for conv2d, fully_connected and max_pool2d.
+    with slim.arg_scope([slim.conv2d, slim.fully_connected, slim.max_pool2d],
+                        outputs_collections=end_points_collection):
+      net = slim.repeat(inputs, 2, slim.conv2d, 64, [3, 3], scope='conv1')
+      net = slim.max_pool2d(net, [2, 2], scope='pool1')
+      net = slim.repeat(net, 2, slim.conv2d, 128, [3, 3], scope='conv2')
+      net = slim.max_pool2d(net, [2, 2], scope='pool2')
+      net = slim.repeat(net, 4, slim.conv2d, 256, [3, 3], scope='conv3')
+      net = slim.max_pool2d(net, [2, 2], scope='pool3')
+      net = slim.repeat(net, 4, slim.conv2d, 512, [3, 3], scope='conv4')
+      net = slim.max_pool2d(net, [2, 2], scope='pool4')
+      net = slim.repeat(net, 4, slim.conv2d, 512, [3, 3], scope='conv5')
+      net = slim.max_pool2d(net, [2, 2], scope='pool5')
+      
+      end_points = slim.utils.convert_collection_to_dict(end_points_collection)
+     
+      return end_points
+
 vgg_19.default_image_size = 224
 
 # Alias
